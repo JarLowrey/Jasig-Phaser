23d4554df66e3803e8d804675ab8561c5f911d62
James <jtronlabs@gmail.com>
2016-08-18 19:09:05 -0500

    added functions to UpgradableStoreItem. Tried to get progressbar working with graphics and roundedRects, but ran into issue with setPercent. It's not going to work without crop being applicable on graphics

--- src/objects/UI/UpgradableStoreItem.js
+++ src/objects/UI/UpgradableStoreItem.js
@@ -4,25 +4,22 @@
 
 export default class UpgradableStoreItem extends Phaser.Group {
 
-  constructor(game, width = 20, height = 100, maxUpgradesNum, currentUpgradeNum, upgradePicKey, upgradePicFrame) {
+  constructor(game, width = 20, height = 100, currentUpgradeNum, maxUpgradesNum, upgradePicKey, upgradePicFrame) {
     super(game);
 
     this.maxUpgradesNum = maxUpgradesNum;
     this.currentUpgradeNum = currentUpgradeNum;
     this.upgradedColor = '0x4df651';
-    this.notYetUpgradedColor = '0x6b5e3e';
+    this.notYetUpgradedColor = '0x705D56';
 
     this.marginFromSides = 10;
 
     this.upgradePicKey = upgradePicKey;
     this.upgradePicFrame = upgradePicFrame;
 
-    this.groupWidth = width;
-    this.groupHeight = height;
-
     //setup background
-    this.backgroundGraphic = game.add.graphics(0, -this.groupHeight / 2);
-    this.setBackgroundGraphicProperties();
+    this.backgroundGraphic = game.add.graphics(0, - height / 2);
+    this.setBackgroundGraphicProperties(width, height);
     this.addChild(this.backgroundGraphic);
 
     //add the upgrade image
@@ -32,38 +29,37 @@ export default class UpgradableStoreItem extends Phaser.Group {
 
     //add the upgrade fillers
     this.upgradeBullets = [];
-    const totalHeightForBullets = this.groupHeight - this.icon.height - this.marginFromSides * 3;//margin needed above and below the icon, and below the top of this group
-    const bulletHeightPlusMargin = totalHeightForBullets / maxUpgradesNum;
-    const bulletHeight = bulletHeightPlusMargin / 2;
-    const bulletMargin = bulletHeightPlusMargin - bulletHeight;
-
-    const bmpData = this.getUpgradeBitmapData(this.groupWidth / 2, bulletHeightPlusMargin / 2);
-    for(var i=0; i<maxUpgradesNum; i++){
-      this.addUpgradeBullet(bmpData, bulletHeight, bulletMargin, i);
-    }
+    this.setNumberUpgradeBullets(currentUpgradeNum, maxUpgradesNum);
   }
 
-  setBackgroundGraphicProperties(lineWidth = 5, color = '0xffd900', radius = this.groupWidth / 3){
+  setBackgroundGraphicProperties(width, height, lineWidth = 5, outlineColor = '0x99E1D9', backgroundColor = '0x332292F', radius = width / 2){
     this.backgroundGraphic.anchor.setTo(0.5,0.5);
+    radius = Math.min(radius, width / 2 - 1, height / 2 - 1); //cannot be >= Max_Dimension / 2
 
+    //draw outline
     this.backgroundGraphic.boundsPadding = 0;
-    this.backgroundGraphic.lineStyle(lineWidth, color, 1);
-    this.backgroundGraphic.drawRoundedRect(0, 0, this.groupWidth, this.groupHeight, radius);
+    this.backgroundGraphic.lineStyle(lineWidth, outlineColor, 1);
+    this.backgroundGraphic.drawRoundedRect(0, 0, width, height, radius);
+
+    //draw fill
+    this.backgroundGraphic.beginFill(backgroundColor);
+    this.backgroundGraphic.drawRoundedRect(0, 0, width, height, radius);
+    this.backgroundGraphic.endFill();
   }
 
   setIconProperties(){
     this.icon.anchor.setTo(0.5,0.5);
-    this.icon.width = this.groupWidth * 0.75;
+    this.icon.width = this.width * 0.75;
     this.icon.height = this.icon.width;
-    this.icon.x = this.groupWidth / 2;
-    this.icon.y = this.groupHeight / 2 - this.icon.height / 2 - this.marginFromSides;
+    this.icon.x = this.width / 2;
+    this.icon.y = this.height / 2 - this.icon.height / 2 - this.marginFromSides;
   }
 
   changeIconTexture(key,frame){
     this.icon.loadTexture(key,frame);
   }
 
-  getUpgradeBitmapData(width, height){
+  getUpgradeBulletBitmapData(width, height){
     var bmd = this.game.add.bitmapData(width, height);
     bmd.ctx.fillStyle = '#ffffff'; //bar must have pure white bitmap data in order to be tinted effectively
     bmd.ctx.beginPath();
@@ -73,6 +69,43 @@ export default class UpgradableStoreItem extends Phaser.Group {
     return bmd;
   }
 
+  incrementUpgrade(){
+    this.currentUpgradeHasChanged(this.upgradedColor);
+    this.currentUpgradeNum++;
+  }
+  decrementUpgrade(){
+    this.currentUpgradeNum--;
+    this.currentUpgradeHasChanged(this.notYetUpgradedColor);
+  }
+  currentUpgradeHasChanged(color){
+    var bullet = this.upgradeBullets[this.currentUpgradeNum];
+    bullet.tint = color;
+    //TODO add tween
+  }
+
+  setNumberUpgradeBullets(currentUpgradeNum = this.currentUpgradeNum, maxUpgradesNum = this.maxUpgradesNum){
+    //remove any current bullets.
+    for(var i=this.upgradeBullets.length-1;i>=0;i--){
+      var oldBullet = this.upgradeBullets.pop();
+      oldBullet.kill();
+    }
+
+    //set vars that govern how many bullets are set
+    this.currentUpgradeNum = currentUpgradeNum;
+    this.maxUpgradesNum = maxUpgradesNum;
+
+    //create the new bullets
+    const totalHeightForBullets = this.height - this.icon.height - this.marginFromSides * 3;//margin needed above and below the icon, and below the top of this group
+    const bulletHeightPlusMargin = totalHeightForBullets / this.maxUpgradesNum;
+    const bulletHeight = bulletHeightPlusMargin * 0.75;
+    const bulletMargin = bulletHeightPlusMargin - bulletHeight;
+
+    const bmpData = this.getUpgradeBulletBitmapData(this.width / 2, bulletHeightPlusMargin / 2);
+    for(i=0; i<this.maxUpgradesNum; i++){
+      this.addUpgradeBullet(bmpData, bulletHeight, bulletMargin, i);
+    }
+  }
+
   addUpgradeBullet(bmpData, bulletHeight, bulletMargin, bulletNo){
     var bullet = this.game.add.image(0,0, bmpData);
     bullet.anchor.setTo(0.5,0.5);
@@ -80,7 +113,7 @@ export default class UpgradableStoreItem extends Phaser.Group {
     this.addChild(bullet);
 
     bullet.x = this.icon.x;
-    bullet.y = this.icon.top - (bulletHeight + bulletMargin) * bulletNo - bulletMargin;
+    bullet.y = this.icon.top - (bulletHeight + bulletMargin) * bulletNo - this.marginFromSides;
 
     bullet.tint = (bulletNo < this.currentUpgradeNum) ? this.upgradedColor : this.notYetUpgradedColor;
   }
--- src/states/Store.js
+++ src/states/Store.js
@@ -10,9 +10,11 @@ export default class Store extends Phaser.State {
   create() {
     this.text = this.game.add.text(this.game.world.centerX,this.game.world.centerY,'Store',{'fill':'#ffffff'});
 
-    var item = new UpgradableStoreItem(this.game, 75, 200, 15, 6, 'sprites', 'upgrade_inc_gun');
-    item.x = this.game.world.centerX / 2;
+    var item = new UpgradableStoreItem(this.game, 75, 200, 6, 15, 'sprites', 'upgrade_inc_gun');
+    item.x = this.game.world.centerX;
     item.y = this.game.world.centerY;
+    item.setNumberUpgradeBullets(10,20);
+    item.decrementUpgrade();
 
     const healthbarJson = this.game.dimen['game_health'];
     var bar = new ProgressBar(this.game);
